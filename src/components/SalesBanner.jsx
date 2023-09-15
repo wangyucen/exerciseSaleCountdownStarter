42d9ea9ca8d4f11270de2e90589f8689eb074057
wangyucen <43635632+wangyucen@users.noreply.github.com>
2023-09-15 13:09:38 +0200

    Initial commit

---
+++ src/components/SalesBanner.jsx
@@ -0,0 +1,12 @@
+function SaleBanner() {
+  // Replace with you component logic
+  return (
+    <>
+      {/* 
+        Replace with your UI logic
+       */}
+    </>
+  );
+}
+
+export default SaleBanner;
